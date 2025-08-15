afbe5749f9ae5458a3d1e576cdce40720d62ebcf
Raman095 <raman5moni11@gmail.com>
2025-08-15 21:39:41 +0530

    JPA Tutorial

---
+++ JPA-tutorial/src/test/java/JPAtutorial/example/JPA/tutorial/JpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package JPAtutorial.example.JPA.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
